f36531e0a4f1c0cf97d87c49395e9f9417d61a71
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 16:22:48 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    Crea dos tablas visualmente (tema libre) en relación de uno a muchos (1:n) (ejemplo, concesionario y coches), añade propiedades (columnas) y conviértelas a código mediante forward engineer.
+    Prueba a guardar también el código sql y reabrirlo.
+    Una vez tengas la base de datos en DBeaver, haz inserciones (al menos 5), asegurándote de insertar correctamente los id de las claves foráneas (en el ejemplo, cada coche en qué concesionario está).
+    Y realiza consultas multitabla, tanto las que vimos anteriormente como algunas nuevas opciones con CONCAT, FORMAT y LIMIT.
+    */
+    }
+}
